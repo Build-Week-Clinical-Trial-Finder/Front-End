7e674022a58237647bcddedc66aa6e875232e80f
Jimmy Hooker <jimmyjjwalker@gmail.com>
2019-10-16 20:21:24 -0700

    Basic state and looping through things - auth with axios

---
+++ trialstarter/src/Components/StudyList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axiosWithAuth from "./axiosAuth.js";
+
+export default function StudyList() {
+
+    const [studies, setStudies] = useState([]);
+
+    useEffect(() => {
+    axiosWithAuth()
+      .get("https://build-clinical-trial-finder.herokuapp.com/api/watchlist/")
+      .then(result => {
+        console.log(result);
+        setStudies(result.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }, []);
+
+    return (
+        <div>
+            {studies.map(study => {
+                console.log(study);
+            })}
+        </div>
+    )
+}
