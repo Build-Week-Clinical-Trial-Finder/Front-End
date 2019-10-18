2385cea742ef77f8b9f5011a9ceaa70642a18ead
Austin637 <austinesparza@gmail.com>
2019-10-17 20:16:15 -0700

    import login in app.js

--- trialstarter/src/App.js
+++ trialstarter/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StudyList from "./Components/StudyList.js";
+import Login from './Components/Login.js';
 
 function App() {
   return (
